4dbf0ad84197bf611d55b5352de15fcf90dd7847
Victor Conde <168321661+ocondeoficial@users.noreply.github.com>
2025-01-14 14:16:47 -0300

    Add files via upload

---
+++ hanoi.py
@@ -0,0 +1,11 @@
+def torre_de_hanoi(entrada, origem, auxiliar, destino):
+    if entrada == 1:
+        print(f"mova o disco {entrada} de {origem} para {destino}")
+    else:
+        torre_de_hanoi(entrada-1, origem, destino, auxiliar)
+        print(f"mova o disco {entrada} de {origem} para {destino}")
+        torre_de_hanoi(entrada-1, auxiliar, origem, destino)
+
+print("digite um número de 1 a 9")
+entrada = int(input())
+torre_de_hanoi(entrada, 'A', 'B', 'C')
